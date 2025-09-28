64c89cb9ca983a6ba69c900cacb1465bb7b4c794
Your Name <shahbajhussain@gmail.com>
2025-09-28 10:53:19 +0530

    all done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { assets } from "../assets/assets";
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { DoctorContext } from "../context/DoctorContext";
 
 export const Navbar = () => {
@@ -25,6 +25,7 @@ export const Navbar = () => {
       <div className="flex items-center gap-2 text-xs">
         <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} />
         <p className="border px-2.5 py-0.5 rounded-full border-gray-600">{aToken ? "Admin" : "Doctor"}</p>
+         <Link className="border text-md px-2.5 py-0.5 rounded-full border-gray-600" to='https://doctors-appointment-frontend-alpha.vercel.app/'>Go to User Panel</Link>
       </div>
 
       <button onClick={ logout} className="bg-cyan-600 text-white text-sm px-10 py-2 rounded-full cursor-pointer hover:bg-slate-500">Logout</button>
